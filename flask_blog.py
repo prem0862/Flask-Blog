627c055b0bc236fb6e7e872eb224c51e16a2614e
= <prem.singh@elucidata.io>
2020-09-15 17:37:52 +0530

    populating text in thml

--- flask_blog.py
+++ flask_blog.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Prem Singh',
+        'title': "Android Devlopement",
+        'content': "I used to do android developement during my initial years",
+        'date': '2020-09-13'
+    },
+    {
+        'author': 'Prem Singh',
+        'title': "Android Devlopement",
+        'content': "I used to do android developement during my initial years",
+        'date': '2020-09-13'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
